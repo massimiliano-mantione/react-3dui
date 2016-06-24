e4b13b0951f1294acfb67a1fd30365b3129c2dcb
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2016-06-24 08:09:44 +0200

    Support for css-layout is working (uses a custom react-konva, see my related fork and pull request).

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Konva from 'konva'
-import {Layer, Rect, Stage} from 'react-konva'
+import {Layer, Rect, Stage} from '../lib/react-konva'
 
 class MyRect extends React.Component {
   constructor (...args) {
@@ -19,13 +19,25 @@ class MyRect extends React.Component {
   render () {
     return (
       <Rect
-        x={10} y={10} width={50} height={50}
         fill={this.state.color}
         shadowBlur={10}
         onClick={this.handleClick}
+        style={this.props.style || {}}
       />
     )
   }
+}
+
+var MARGIN = 5
+var stageStyle = {}
+var layerStyle = {flexDirection: 'column', alignItems: 'stretch', flex: 1}
+var shapeStyle = {
+  flex: 1,
+  margin: MARGIN
+}
+var shapeStyle2 = {
+  flex: 2,
+  margin: MARGIN
 }
 
 function App () {
@@ -33,9 +45,12 @@ function App () {
   // Layer - is a <canvas> element on the page
   // so you can use several canvases. It may help you to improve performance a lot.
   return (
-    <Stage width={700} height={700}>
-      <Layer>
-          <MyRect/>
+    <Stage width={700} height={700} style={stageStyle}>
+      <Layer style={layerStyle}>
+          <MyRect style={shapeStyle}/>
+          <MyRect style={shapeStyle}/>
+          <MyRect style={shapeStyle}/>
+          <MyRect style={shapeStyle2}/>
       </Layer>
     </Stage>
   )

bf3e15b4eff38f048335114b5e5c001e6a84a2b6
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2016-06-25 18:34:45 +0200

    Added basic akkad canvas.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Konva from 'konva'
 import {Layer, Rect, Stage} from '../lib/react-konva'
+import {Akkad, Scene, cameras, lights, shapes} from 'akkad'
 
 class MyRect extends React.Component {
   constructor (...args) {
@@ -40,19 +41,43 @@ var shapeStyle2 = {
   margin: MARGIN
 }
 
+const {ArcRotateCamera} = cameras
+const {HemisphericLight} = lights
+const {Box} = shapes
+
+class App3d extends React.Component {
+  render () {
+    return (
+      <Akkad>
+        <Scene>
+          <ArcRotateCamera
+            position={[3, 4, -5]}
+            target={[0, 1, 0]}
+          />
+          <HemisphericLight />
+          <Box />
+        </Scene>
+      </Akkad>
+    )
+  }
+}
+
 function App () {
   // Stage - is a div wrapper
   // Layer - is a <canvas> element on the page
   // so you can use several canvases. It may help you to improve performance a lot.
   return (
-    <Stage width={700} height={700} style={stageStyle}>
-      <Layer style={layerStyle}>
-          <MyRect style={shapeStyle}/>
-          <MyRect style={shapeStyle}/>
-          <MyRect style={shapeStyle}/>
-          <MyRect style={shapeStyle2}/>
-      </Layer>
-    </Stage>
+    <div>
+      <Stage width={300} height={300} style={stageStyle}>
+        <Layer style={layerStyle}>
+            <MyRect style={shapeStyle}/>
+            <MyRect style={shapeStyle}/>
+            <MyRect style={shapeStyle}/>
+            <MyRect style={shapeStyle2}/>
+        </Layer>
+      </Stage>
+      <App3d/>
+    </div>
   )
 }
 

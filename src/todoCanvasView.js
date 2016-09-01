ca2a40731e4d57b567300dce6c02e8d8909ab9dc
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2016-09-01 07:45:48 +0200

    WIP on canvas event handling, done with styling.

--- src/todoCanvasView.js
+++ src/todoCanvasView.js
@@ -4,14 +4,15 @@ import {Styler} from 'debonair'
 
 import {listElementScale, listElementPosition} from './listElementScale'
 
-const ROW_HEIGTH = 20
-const LINE_HEIGTH = 25
+const ROW_HEIGTH = 25
+const LINE_HEIGTH = 30
+const GAP_HEIGTH = 10
 const LIST_SIZE = 3.50
 const LIST_HEIGTH = LIST_SIZE * ROW_HEIGTH
 const HEADER_HEIGTH = LINE_HEIGTH
 const FOOTER_HEIGTH = LINE_HEIGTH
 const PANEL_WIDTH = 300
-const PANEL_HEIGTH = HEADER_HEIGTH + LIST_HEIGTH + FOOTER_HEIGTH
+const PANEL_HEIGTH = HEADER_HEIGTH + LIST_HEIGTH + FOOTER_HEIGTH + (3 * GAP_HEIGTH)
 const BACKGROUND_COLOR = 'white'
 const BACKGROUND_LINE_COLOR = 'grey'
 const TEXT_COLOR = 'black'
@@ -21,13 +22,8 @@ let createStyler = Styler.create
 let stageStyler = createStyler({})
 let flexStyler = createStyler({flex: 1})
 let containerStyler = createStyler(flexStyler, {alignItems: 'stretch'})
-let columnStyler = createStyler(containerStyler, {flexDirection: 'column'})
-let flatStyler = createStyler({flat: true})
-let panelColorStyler = createStyler({flat: true})
-let listStyler = createStyler({flat: true, height: LIST_SIZE * ROW_HEIGTH})
 let rowStyler = createStyler(containerStyler, {flexDirection: 'row'})
-let marginStyler = createStyler({margin: 5})
-let panelItemStyler = createStyler(containerStyler, marginStyler)
+let panelItemStyler = createStyler(containerStyler)
 
 let PanelRow = React.createClass({
   propTypes: {
@@ -37,11 +33,12 @@ let PanelRow = React.createClass({
     height: React.PropTypes.number.isRequired,
     fill: React.PropTypes.string,
     stroke: React.PropTypes.string,
-    dispatch: React.PropTypes.func.isRequired
+    dispatch: React.PropTypes.func.isRequired,
+    onClick: React.PropTypes.func
   },
 
   render: function () {
-    let {y, scaleY, width, height, fill, stroke, children, dispatch} = this.props
+    let {y, scaleY, width, height, fill, stroke, children, dispatch, onClick} = this.props
     if (scaleY === undefined) {
       scaleY = 1
     }
@@ -56,7 +53,7 @@ let PanelRow = React.createClass({
     }
     return (
       <Group y={y} scaleY={scaleY}>
-        <Rect fill={fill} stroke={stroke} height={height} width={width} cornerRadius={4}/>
+        <Rect onClick={onClick} fill={fill} stroke={stroke} height={height} width={width} cornerRadius={5}/>
         <Group style={containerStyler({height: height, width: width})}>
           {children}
         </Group>
@@ -66,11 +63,13 @@ let PanelRow = React.createClass({
 })
 
 let PanelHeader = React.createClass({
-  propTypes: {},
+  propTypes: {
+    onClick: React.PropTypes.func
+  },
   render: function () {
-    let {children} = this.props
+    let {children, onClick} = this.props
     return (
-      <PanelRow y={0} fill={BACKGROUND_LINE_COLOR} stroke={BACKGROUND_LINE_COLOR} height={HEADER_HEIGTH} width={PANEL_WIDTH}>
+      <PanelRow onClick={onClick} y={GAP_HEIGTH} fill={BACKGROUND_LINE_COLOR} stroke={BACKGROUND_LINE_COLOR} height={HEADER_HEIGTH} width={PANEL_WIDTH}>
         {children}
       </PanelRow>
     )
@@ -78,12 +77,13 @@ let PanelHeader = React.createClass({
 })
 let PanelFooter = React.createClass({
   propTypes: {
+    onClick: React.PropTypes.func,
     y: React.PropTypes.number.isRequired
   },
   render: function () {
-    let {y, children} = this.props
+    let {y, children, onClick} = this.props
     return (
-      <PanelRow y={y} fill={BACKGROUND_LINE_COLOR} stroke={BACKGROUND_LINE_COLOR} height={HEADER_HEIGTH} width={PANEL_WIDTH}>
+      <PanelRow onClick={onClick} y={y} fill={BACKGROUND_LINE_COLOR} stroke={BACKGROUND_LINE_COLOR} height={HEADER_HEIGTH} width={PANEL_WIDTH}>
         {children}
       </PanelRow>
     )
@@ -103,18 +103,49 @@ let PanelBody = React.createClass({
   }
 })
 
-let PanelText = React.createClass({
+let HeaderText = React.createClass({
   propTypes: {
+    onClick: React.PropTypes.func,
     text: React.PropTypes.string.isRequired,
-    fontSize: React.PropTypes.number
+    align: React.PropTypes.string
   },
   render: function () {
-    let {text, fontSize} = this.props
-    if (typeof fontSize !== 'number') {
-      fontSize = 14
+    let {text, align, onClick} = this.props
+    if (typeof align !== 'string') {
+      align = 'center'
     }
+    if (align === 'left') {
+      text = ' ' + text
+    }
+    if (align === 'right') {
+      text = text + ' '
+    }
+    let fontSize = 16
     return (
-      <Text style={panelItemStyler()} text={text} fill={TEXT_COLOR} align={'left'} fontSize={fontSize}/>
+      <Text onClick={onClick} style={panelItemStyler()} text={text} fill={TEXT_COLOR} align={align} fontSize={fontSize} offsetY={(fontSize / 2) - (LINE_HEIGTH / 2)}/>
+    )
+  }
+})
+let TodoText = React.createClass({
+  propTypes: {
+    onClick: React.PropTypes.func,
+    text: React.PropTypes.string.isRequired,
+    align: React.PropTypes.string
+  },
+  render: function () {
+    let {text, align, onClick} = this.props
+    if (typeof align !== 'string') {
+      align = 'center'
+    }
+    if (align === 'left') {
+      text = ' ' + text
+    }
+    if (align === 'right') {
+      text = text + ' '
+    }
+    let fontSize = 14
+    return (
+      <Text onClick={onClick} style={panelItemStyler()} text={text} fill={TEXT_COLOR} align={align} fontSize={fontSize} offsetY={(fontSize / 2) - (ROW_HEIGTH / 2)}/>
     )
   }
 })
@@ -129,26 +160,14 @@ let TodoElement = React.createClass({
 
   render: function () {
     let {y, scale, todo, dispatch} = this.props
+    let clickHandler = () => dispatch('toggle', todo.id)
     return (
-      <PanelRow y={y} scaleY={scale} fill={BACKGROUND_COLOR} stroke={BACKGROUND_LINE_COLOR} height={ROW_HEIGTH} width={PANEL_WIDTH}>
+      <PanelRow onClick={clickHandler} y={y} scaleY={scale} fill={BACKGROUND_COLOR} stroke={BACKGROUND_LINE_COLOR} height={ROW_HEIGTH} width={PANEL_WIDTH}>
         <Group style={rowStyler()} height={ROW_HEIGTH}>
-          <Text style={panelItemStyler()} text={todo.text} fill={'black'} align={'left'} fontSize={14}/>
-          <Text style={panelItemStyler()} text={todo.done ? 'DONE' : 'TODO'} fill={'black'} align={'center'} fontSize={14}/>
-          <Text style={panelItemStyler()} text={'TOGGLE'} fill={'black'} align={'center'} fontSize={14} onClick={() => dispatch('toggle', todo.id)}/>
+          <TodoText onClick={clickHandler} text={todo.text} align={'left'}/>
+          <TodoText onClick={clickHandler} text={todo.done ? '\u2713' : '\u2613 '} align={'right'}/>
         </Group>
       </PanelRow>
-    )
-  },
-  renderXXX: function () {
-    let {y, scale, todo, dispatch} = this.props
-    return (
-      <Group style={{flat: true, height: ROW_HEIGTH, top: y}} scaleY={scale} y={y}>
-        <Group style={rowStyler()} height={ROW_HEIGTH}>
-          <Text style={panelItemStyler()} text={todo.text} fill={'black'} align={'left'} fontSize={14}/>
-          <Text style={panelItemStyler()} text={todo.done ? 'DONE' : 'TODO'} fill={'black'} align={'center'} fontSize={14}/>
-          <Text style={panelItemStyler()} text={'TOGGLE'} fill={'black'} align={'center'} fontSize={14} onClick={() => dispatch('toggle', todo.id)}/>
-        </Group>
-      </Group>
     )
   }
 })
@@ -160,15 +179,15 @@ let TodoPanel = React.createClass({
 
   render: function () {
     let dispatch = this.props.dispatch
+    let clickHandler = () => dispatch('removeDone')
     let state = this.props.dispatch('getState')
 
-    //
     return (
-      <PanelRow fill={BACKGROUND_COLOR} stroke={BACKGROUND_LINE_COLOR} height={PANEL_HEIGTH} width={PANEL_WIDTH}>
+      <Group>
         <PanelHeader>
-          <PanelText text={state.text} fontSize={14}/>
+          <HeaderText text={state.text} align={'left'}/>
         </PanelHeader>
-        <PanelBody y={HEADER_HEIGTH}>
+        <PanelBody y={HEADER_HEIGTH + (2 * GAP_HEIGTH)}>
           {
             state.todos.reduce((todos, todo, todoIndex) => {
               let scale = listElementScale(LIST_SIZE, state.scroll, todoIndex)
@@ -181,45 +200,10 @@ let TodoPanel = React.createClass({
             }, [])
           }
         </PanelBody>
-        <PanelFooter y={PANEL_HEIGTH + LIST_HEIGTH}>
-          <PanelText text={'REMOVE DONE'} fontSize={14}/>
+        <PanelFooter onClick={clickHandler} y={HEADER_HEIGTH + LIST_HEIGTH + (3 * GAP_HEIGTH)}>
+          <HeaderText onClick={clickHandler} text={'REMOVE DONE'} align={'center'}/>
         </PanelFooter>
-      </PanelRow>
-    )
-  },
-
-  renderXX: function () {
-    let dispatch = this.props.dispatch
-    let state = this.props.dispatch('getState')
-
-    //
-    return (
-      <PanelRow fill={BACKGROUND_COLOR} stroke={BACKGROUND_LINE_COLOR} height={PANEL_HEIGTH} width={PANEL_WIDTH}>
-        <PanelRow y={0} fill={BACKGROUND_LINE_COLOR} stroke={BACKGROUND_LINE_COLOR} height={HEADER_HEIGTH} width={PANEL_WIDTH}>
-          <Text style={panelItemStyler()} text={state.text} fill={TEXT_COLOR} align={'left'} fontSize={14}/>
-        </PanelRow>
-        <PanelRow y={HEADER_HEIGTH} fill='green' stroke='blue' height={HEADER_HEIGTH} width={PANEL_WIDTH}>
-          <Text style={panelItemStyler()} text={'HOLA'} fill={'black'} align={'left'} fontSize={14}/>
-        </PanelRow>
-        {withBackground(0, HEADER_HEIGTH, 'blue', 'green',
-          <Text style={panelItemStyler()} text={state.text} fill={'black'} align={'left'} fontSize={14} height={ROW_HEIGTH}/>
-        )}
-        {withBackground(HEADER_HEIGTH, LIST_HEIGTH, 'blue', 'green',
-          state.todos.reduce((todos, todo, todoIndex) => {
-            let scale = listElementScale(LIST_SIZE, state.scroll, todoIndex)
-            let y = listElementPosition(LIST_SIZE, state.scroll, todoIndex) * ROW_HEIGTH
-            if (todoIndex === 0) {
-              console.log(' *** state.todos.reduce START', LIST_SIZE, state.scroll)
-            }
-            console.log('     ', todoIndex, scale, y)
-            if (scale > 0) {
-              todos.push(<TodoElement todo={todo} dispatch={dispatch} key={todo.id} scale={scale} y={y}/>)
-            }
-            return todos
-          }, [])
-        )}
-        <Text style={panelItemStyler()} text={'REMOVE DONE'} fill={'black'} align={'center'} fontSize={14} height={ROW_HEIGTH} onClick={() => dispatch('removeDone')}/>
-      </PanelRow>
+      </Group>
     )
   }
 })
